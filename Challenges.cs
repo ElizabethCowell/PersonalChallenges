4faeefa1f6b7e01f99cc16bbb078eabffb8cfe8c
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-05-09 21:55:46 -0400

    TDD exercise

---
+++ Challenges.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalChallenges
+{
+    public class Challenges
+    {
+        public bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+
+        public int FindAverage(int x, int y, int z)
+        {
+            return (x + y + z) / 3;
+        }
+
+        public bool ContainsThe(string word)
+        {
+            return word.Contains("the");
+        }
+
+        public bool password(string password)
+        {
+            return (password.Length >= 8 &&
+            password.Any(char.IsUpper) &&
+            password.Any(char.IsLower) &&
+            password.Any(char.IsDigit));        
+        }
+        
+        public string mealPlanning(string DayOfTheWeek)
+        {
+            switch (DayOfTheWeek)
+            {
+                case "Monday":
+                        return "Meatless Monday";
+                case "Tuesday":
+                    return "Taco Tuesday";
+                case "Wednesday":
+                    return "Italian Wednesday";
+                case "Thursday":
+                    return "Curry Thursday";
+                case "Friday":
+                    return "Stir-Friday";
+                default:
+                    return "Leftovers";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
